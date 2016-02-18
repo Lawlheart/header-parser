6b54a89141b78e87f6d60a44a0aeba1068db18d7
LawlietBlack <kenneth.black@live.com>
2016-02-18 12:32:38 -0800

    fixed package.json

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,10 +10,15 @@ module.exports = function (app, db) {
       res.sendFile(process.cwd() + '/public/index.html');
     });
 
+  app.route('/api/whoami')
+    .get(function(req, res) {
+      console.log(req.headers)
+      res.json(req.headers)
+    });
 
 
-  app.route('/api/clicks')
-    .get(clickHandler.getClicks)
-    .post(clickHandler.addClick)
-    .delete(clickHandler.resetClicks);
+  // app.route('/api/clicks')
+  //   .get(clickHandler.getClicks)
+  //   .post(clickHandler.addClick)
+  //   .delete(clickHandler.resetClicks);
 };
